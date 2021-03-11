42186968f4d315112aa4a9a86cd7ede79db8ba6c
Vinicius Melo <viniiciiusmelo@gmail.com>
2021-03-11 00:47:26 -0300

    Commit 2

--- app/src/main/java/com/example/besmart/CrieIdeiaFragment.java
+++ app/src/main/java/com/example/besmart/CrieIdeiaFragment.java
@@ -2,41 +2,42 @@ package com.example.besmart;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.besmart.models.ModelClass_Ideia;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Calendar;
+import java.util.Date;
 
 
 public class CrieIdeiaFragment extends Fragment {
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-
-    private String mParam1;
-    private String mParam2;
+    private EditText tituloIdeia;
+    private EditText descripIdeia;
+    private MaterialButton save;
 
     public CrieIdeiaFragment() {
         // Required empty public constructor
     }
 
-    public static CrieIdeiaFragment newInstance(String param1, String param2) {
-        CrieIdeiaFragment fragment = new CrieIdeiaFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
@@ -44,6 +45,48 @@ public class CrieIdeiaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_crie_ideia, container, false);
+        View v = inflater.inflate(R.layout.fragment_crie_ideia, container, false);
+
+        FirebaseFirestore fb = FirebaseFirestore.getInstance();
+        FirebaseAuth auth  = FirebaseAuth.getInstance();
+
+        tituloIdeia = v.findViewById(R.id.titlo_text);
+        descripIdeia = v.findViewById(R.id.edit_descrip);
+        save = v.findViewById(R.id.button_save_ideia);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Date data = new Date();
+
+                if(tituloIdeia.getText().toString().isEmpty()) {
+                    tituloIdeia.requestFocus();
+                    tituloIdeia.setError("Campo obrigatório");
+                }else if(descripIdeia.getText().toString().isEmpty()){
+                    descripIdeia.requestFocus();
+                    descripIdeia.setError("Campo obrigatório");
+                }else{
+
+                    ModelClass_Ideia ideia = new ModelClass_Ideia(tituloIdeia.getText().toString(),descripIdeia.getText().toString(),"Disponivel",data.getTime());
+
+                    //pensar melhor e um ID para a Ideia PodeSer O nomeDo Usuario+OnomeDaIdeia
+                    fb.collection("User").document(auth.getCurrentUser().getUid()).collection("Ideias").document(ideia.getTitulo()).set(ideia)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    Toast.makeText(getContext(), "Ideia Salva", Toast.LENGTH_SHORT).show();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getContext(), "Error ao salvar Ideia", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
+
+        return v;
     }
 }
--- app/src/main/java/com/example/besmart/models/ModelClass_Ideia.java
+++ app/src/main/java/com/example/besmart/models/ModelClass_Ideia.java
@@ -1,4 +1,52 @@
 package com.example.besmart.models;
 
+import java.util.Date;
+
 public class ModelClass_Ideia {
+
+    private String titulo;
+    private String descricao;
+    private String status;
+    private long dataInscricao;
+
+    public ModelClass_Ideia(){}
+
+    public ModelClass_Ideia(String titulo, String descricao, String status, long dataInscricao) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.status = status;
+        this.dataInscricao = dataInscricao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public long getDataInscricao() {
+        return dataInscricao;
+    }
+
+    public void setDataInscricao(long dataInscricao) {
+        this.dataInscricao = dataInscricao;
+    }
 }
